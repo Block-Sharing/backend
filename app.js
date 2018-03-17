453746a62c64283950f1a8f0b166fb627a6c1d4a
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2018-03-17 21:43:51 +0100

    return items, refactoring with await

--- app.js
+++ app.js
@@ -80,29 +80,34 @@ app.post('/persons', (req, res) => {
     })
 });
 
-app.post('/houses/:houseHash/item', async (req, res) => {
+app.post('/houses/:houseHash/items', async (req, res) => {
     let data = req.body;
     const houseHash = req.params.houseHash;
-    const userId = req.get('X-UserHash');
-    console.log('Creating new Item for User ' + userId + ' for House' );
+    const userHash = req.get('X-UserHash');
+    console.log('Creating new Item for User ' + userHash + ' for House' );
+    let person = await Person.findOne({hash: userHash}).exec();
+    data.owner = person._id;
     const item = new Item(data);
 
-    item.save(err => {
-        if (err) {
-            res.status(500).json(err);
-        } else {
-            House.findOne({hash: houseHash}).exec((err, house) => {
-                if (house.items) {
-                    house.items.push(item._id);
-                } else {
-                    house.items = [item._id];
-                }
-                house.save(err => {
-                    res.json(item);
-                })
-            });
-        }
-    })
+    await item.save();
+    let house = await House.findOne({hash: houseHash}).exec();
+    house.items = house.items || [];
+    house.items.push(item._id);
+    await house.save();
+    res.json(item);
+});
+
+app.get('/houses/:houseHash/items', async (req, res) => {
+    const houseHash = req.params.houseHash;
+    let house = await House.findOne({hash: houseHash})
+    .populate('items')
+    .exec();
+
+    let items = await Person.populate(house.items, {
+        path: 'owner',
+        select: ['nickname', 'floor']
+    });
+    res.send(items);
 });
 
 app.listen(3000, () => {
